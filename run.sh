7b0ce8e6785faee037a327a43b725c1a154103d0
Jacopo Panerati <jacopo.pan@gmail.com>
2020-03-20 00:26:10 -0400

    comments

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
-if [ -d "./COVID-19" ]
+
+# if dir 'COVID-19' exists
+if [ -d './COVID-19' ]
 then
+	# pull the most recent data
 	cd ./COVID-19
 	git pull
 	cd ..
 else
+	# otherwise, clone it from JHU's repo
 	git clone https://github.com/CSSEGISandData/COVID-19.git
 fi
 
+# run the python script
 python3.7 ./script.py
--- script.py
+++ script.py
@@ -13,16 +13,29 @@ sns.set()
 # SIMPLE PARSING
 ################
 
+# there are 3 time series files
 files = ['Confirmed', 'Deaths', 'Recovered']
 
+# for each one of them
 for s in files:
+	# read the CSV
 	temp = pd.read_csv('./COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-'+s+'.csv') 
+	# drop latitute and longitude information
 	temp = temp.drop('Lat', axis=1)
 	temp = temp.drop('Long', axis=1)
+	# sort by country
 	temp = temp.sort_values(by=['Country/Region'])
+	# remove the entires with a ',' in the 'Province/State' column
+	# as of March 20, these are entires relative US counties
+	# while I only want to consider the US states
 	temp = temp[~temp['Province/State'].str.contains(',', na=False)]
+	# sum lines relative to regions of a single country
+	# i.e., Chinese provinces, Canadian provinces, US states, etc
 	temp = temp.groupby(['Country/Region']).sum()
+	# transpose the frame
 	temp = temp.transpose()
+	# add a 'date column'
+	# as of March, 20 all series start on January, 22
 	temp['date'] = pd.date_range(start='22/1/2020', periods=len(temp), freq='D')
 	if s == 'Confirmed':
 		confirmed = temp
@@ -31,13 +44,22 @@ for s in files:
 	elif s == 'Recovered':
 		recovered = temp
 
+# these 3 dataframes contain information
+# about confirmed cases, deaths, and recovered patients
+# for 155 countries over 58 (as of March 20) days
 print(confirmed['Canada'])
 print(deaths)
 print(recovered)
 
+
+
+
 ################
 # SIMPLE PLOTTING
 ################
+
+# confirmed cases, deaths, and recovered patients for
+# a selection of 5 countries for each of the 5 continents
 
 continents = ['Asia', 'Europe', 'Americas', 'Africa', 'Oceania and others']
 countries = [	'China',		'Italy',			'US',			'South Africa',		'Australia', \
@@ -49,20 +71,23 @@ countries = [	'China',		'Italy',			'US',			'South Africa',		'Australia', \
 fig, axs = plt.subplots(5,5)
 
 for i in range(len(countries)):
+	# get axis
 	ax = axs[int(i/5), i%5]
+	# set title
 	if i<5:
 		ax.set_title(continents[i] + '\n' + countries[i], fontsize=10)
 	else: 
 		ax.set_title(countries[i], fontsize=10)
+	# plot
 	confirmed.plot('date', countries[i], ax=ax, legend=False)
 	deaths.plot('date', countries[i], ax=ax, legend=False)
 	recovered.plot('date', countries[i], ax=ax, legend=False)
+	# label and ticks options
 	ax.set_xlabel('')
 	ax.tick_params(axis='both', which='both', labelsize=6)
+	# only plot the x ticks for the last row of plots
 	if i < 20:
 		ax.set_xticks([], minor=True)
 
 plt.show()
-
-
 
